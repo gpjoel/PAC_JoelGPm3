26e9f4ccf13f2fb447d0be060a13f1560053141c
gpjoel <joel.garciap@educem.net>
2024-05-02 12:27:31 +0200

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,1 +1,25 @@
 print("hola")
+
+diccionari = {}
+
+print(diccionari)
+
+paraula = "xarxa"
+entrada = {
+  "PESCA" : "Ormeig de pescar constituït per un teixit de fils nuats formant una retícula quadrada o rombal."}
+
+diccionari.update({paraula:entrada})
+
+print(diccionari)
+
+auxiliar = diccionari[paraula]
+
+entrada = {
+  "TÈXTIL" : "Teixit de les xarxes de pescar, fabricat amb torçal de cotó o amb fil d’abacà."}
+  
+auxiliar.update(entrada)
+
+
+diccionari.update({paraula:auxiliar})
+
+print(diccionari)
