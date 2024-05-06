c0e50dacfc70c9dd9dfd6f57170fca1cccbf6ccf
gpjoel <joel.garciap@educem.net>
2024-05-06 13:25:03 +0200

    version2

--- prueba.py
+++ prueba.py
@@ -1,25 +1,57 @@
-print("hola")
+# print("hola")
 
-diccionari = {}
+# diccionari = {}
 
-print(diccionari)
+# print(diccionari)
 
-paraula = "xarxa"
-entrada = {
-  "PESCA" : "Ormeig de pescar constituït per un teixit de fils nuats formant una retícula quadrada o rombal."}
+# paraula = "xarxa"
+# entrada = {
+#   "PESCA" : "Ormeig de pescar constituït per un teixit de fils nuats formant una retícula quadrada o rombal."}
 
-diccionari.update({paraula:entrada})
+# diccionari.update({paraula:entrada})
 
-print(diccionari)
+# print(diccionari)
 
-auxiliar = diccionari[paraula]
+# auxiliar = diccionari[paraula]
 
-entrada = {
-  "TÈXTIL" : "Teixit de les xarxes de pescar, fabricat amb torçal de cotó o amb fil d’abacà."}
+# entrada = {
+#   "TÈXTIL" : "Teixit de les xarxes de pescar, fabricat amb torçal de cotó o amb fil d’abacà."}
   
-auxiliar.update(entrada)
+# auxiliar.update(entrada)
 
 
-diccionari.update({paraula:auxiliar})
+# diccionari.update({paraula:auxiliar})
 
-print(diccionari)
+# print(diccionari)
+
+
+
+
+# Creació
+
+persona =	{
+  "nom": "Eustaqui",
+  "llocNaixement": "Andorra",
+  "anyNaixement": 1990
+}
+
+# persona = dict(nom = "Anselm", llocNaixement = "Portugal", anyNaixement = 1991)
+
+
+# Accés
+print(persona["nom"])
+print(persona.get("nom"))
+
+print(persona.keys())
+print(persona.values())
+print(persona.items())
+
+# Afegir
+    # mitjançant index
+    # mitjançant mètode (update())
+
+# Eliminar
+    # mitjançant mètode (pop())
+    # mitjançant mètode (popitem())
+    # mitjançant mètode (clear())
+    # mitjançant paraula clau (del)
