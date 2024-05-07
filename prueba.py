7d63cf4c2558e3a22e108dbb99259a847483066d
gpjoel <joel.garciap@educem.net>
2024-05-07 23:37:38 +0200

    versionfinal

--- prueba.py
+++ prueba.py
@@ -39,9 +39,24 @@ persona =	{
 
 
 # Accés
+"""
+persona["nom"] genera un error si la clave no existe. Esta línea de código accede al valor asociado a la clave "nom" en el diccionario persona y lo imprime
+persona.get("nom") devuelve None si la clave no existe. Esta línea utiliza el método .get() de los diccionarios para obtener el valor asociado a la clave "nom"
+"""
 print(persona["nom"])
 print(persona.get("nom"))
+"""
+persona.keys()  imprime las claves del diccionario persona. El método .keys() devuelve una vista de todas 
+las claves en el diccionario. Es útil cuando necesitas iterar sobre las claves del diccionario o realizar 
+operaciones específicas con ellas.
 
+persona.values() Esta línea imprime los valores del diccionario persona. El método .values() devuelve una 
+vista de todos los valores en el diccionario. Es útil cuando necesitas trabajar solo con los valores y no necesitas las claves.
+
+persona.items() Aquí se imprimen los pares clave-valor del diccionario persona. El método .items() devuelve una vista de tuplas que contienen 
+tanto las claves como los valores del diccionario. Cada tupla en la lista representa un par clave-valor del diccionario.
+
+"""
 print(persona.keys())
 print(persona.values())
 print(persona.items())
@@ -49,24 +64,40 @@ print(persona.items())
 # Afegir
     # mitjançant index
     # mitjançant mètode (update()) The update() method will update the dictionary with the items from a given argument. If the item does not exist, the item will be added.
-
+"""
+El método update() actualizará el diccionario con los elementos de un argumento dado. Si el elemento no existe, se añadirá el elemento.
+"""
 persona.update({"Ubicacio": "Barcelona"})
+"""
+Se añade una entrada al diccionario utilizando una nueva clave de índice y asignando un valor a esta clave:
+"""
 persona["pes"] = 80
 
 # Eliminar
     # mitjançant mètode (pop())
+"""
+persona.pop("llocNaixement") elimina la clave "llocNaixement" junto con su valor asociado del diccionario persona.
+"""
 persona.pop("llocNaixement")
 print(persona)
-    # mitjançant mètode (popitem()) en este caso elimina el utlimo del diccionario
+    # mitjançant mètode (popitem()) 
+"""
+popitem() en este caso elimina  el útlimo término del diccionario
+"""
 
 persona.popitem()
 print(persona)
     # mitjançant paraula clau (del)
 
+"""
+con el del eliminamos algo especificament, en este caso anyNaixement
+"""
 del persona["anyNaixement"]
 print(persona)
 
     # mitjançant mètode (clear()) elimina toda la lista 
-
+"""
+clear()) elimina toda la lista 
+"""
 persona.clear()
 print(persona)  
